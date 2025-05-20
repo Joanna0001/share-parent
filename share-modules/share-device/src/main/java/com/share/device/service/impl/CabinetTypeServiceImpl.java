4b6a789372e985a7ff7e49fa98f9e084fe02949c
Joanna0001 <gkj159@163.com>
2025-05-20 17:30:49 +0800

    device cabinetType

--- share-modules/share-device/src/main/java/com/share/device/service/impl/CabinetTypeServiceImpl.java
+++ share-modules/share-device/src/main/java/com/share/device/service/impl/CabinetTypeServiceImpl.java
@@ -1,2 +1,19 @@
-package com.share.device.service.impl;public class CabinetTypeServiceImpl {
+package com.share.device.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.share.device.domain.CabinetType;
+import com.share.device.mapper.CabinetTypeMapper;
+import com.share.device.service.ICabinetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CabinetTypeServiceImpl extends ServiceImpl<CabinetTypeMapper, CabinetType> implements ICabinetTypeService {
+    @Autowired
+    private CabinetTypeMapper cabinetTypeMapper;
+
+    @Override
+    public List<CabinetType> selectCabinetTypeList(CabinetType cabinetType) {
+        return cabinetTypeMapper.selectCabinetTypeList(cabinetType);
+    }
 }
